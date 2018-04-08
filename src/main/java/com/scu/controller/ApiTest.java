1bb3bccaa003c669ba13eec6b9580139530f5ba7
Neeraj <virusneerajkumarjha@gmail.com>
2018-04-08 14:15:08 +0530

    test

--- src/main/java/com/scu/controller/ApiTest.java
+++ src/main/java/com/scu/controller/ApiTest.java
@@ -1,13 +1,24 @@
 package com.scu.controller;
 
+import com.scu.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiTest {
 
+    @Autowired
+    EmailService emailService;
+
     @GetMapping("/")
     public String test(){
         return "Api is up & running";
+    }
+
+    @PostMapping("/sendEmail")
+    public Boolean sendEmail(){
+        return emailService.sendEmail("virusneerajkumarjha@gmail.com","test","Testing");
     }
 }
